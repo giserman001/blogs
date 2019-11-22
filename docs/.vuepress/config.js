3759f4b7b658d33a1134a16212237e87b55486fa
Liu Ya <1522962872@qq.com>
2019-11-22 20:27:18 +0800

    提交代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,15 @@
 module.exports = {
   base: '/blogs/',
   title: 'blogs',
-  description: 'Vuepress blog'
+  description: 'Vuepress blog',
+  themeConfig: {
+    // 你的GitHub仓库，请正确填写
+    repo: 'https://github.com/xxxxxxx/blog-demo',
+    // 自定义仓库链接文字。
+    repoLabel: 'My GitHub',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+    ]
+  }
 }
