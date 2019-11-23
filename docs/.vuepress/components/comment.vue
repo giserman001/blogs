57868efd228fc28d99641ea0e18ebb4021a8c341
liuya <1522962872@qq.com>
2019-11-23 10:59:07 +0800

    添加博客评论系统

---
+++ docs/.vuepress/components/comment.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "comment",
+  data() {
+    return {};
+  },
+  mounted() {
+    let body = document.querySelector(".gitalk-container");
+    let script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js";
+    body.appendChild(script);
+    script.onload = () => {
+      const commentConfig = {
+        clientID: "a0a3296915c68cd08325",
+        clientSecret: "3fab08fbf66b40338349249c92f65175e3505e1f",
+        repo: "blogs",
+        owner: "giserman001",
+        // 这里接受一个数组，可以添加多个管理员
+        admin: ["giserman001"],
+        // id 用于当前页面的唯一标识，一般来讲 pathname 足够了，
+        // 但是如果你的 pathname 超过 50 个字符，GitHub 将不会成功创建 issue，此情况可以考虑给每个页面生成 hash 值的方法.
+        id: location.pathname,
+        distractionFreeMode: false
+      };
+      const gitalk = new Gitalk(commentConfig);
+      gitalk.render("gitalk-container");
+    };
+  }
+};
+</script>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,35 @@
+const fs = require('fs')
+// 获取该文件夹下的所有文件名
+const getFileNames = (parentFileName) => {
+  const results = []
+  const files = fs.readdirSync(`./docs${parentFileName}`)
+  files.forEach((val) => {
+    if ('README.md'.includes(val)) {
+      // results.push('')
+    } else {
+      results.push(val)
+    }
+  })
+  return results
+}
+
 module.exports = {
   base: '/blogs/',
   title: 'blogs',
   description: 'Vuepress blog',
   themeConfig: {
     // 你的GitHub仓库，请正确填写
-    repo: 'https://github.com/xxxxxxx/blog-demo',
+    repo: 'https://github.com/giserman001',
     // 自定义仓库链接文字。
     repoLabel: 'My GitHub',
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'FirstBlog',
+        link: '/blog/FirstBlog.md'
+      }
     ]
   }
 }
