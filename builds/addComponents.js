d3bc19ee23adaab30ac2a0767e4f460de8ddca10
Liu Ya <1522962872@qq.com>
2019-12-02 17:42:00 +0800

    除了.doc文件根目录下README.md文件不添加评论功能其他页面都添加

--- builds/addComponents.js
+++ builds/addComponents.js
@@ -6,10 +6,14 @@ const rootDir = './docs'
 findMarkdown(rootDir, writeComponents)
 
 function writeComponents(dir) {
-  if (!/README/.test(dir)) {
-    fs.appendFile(dir, `\n \n <comment-comment/> \n `, (err) => {
-      if (err) throw err
-      console.log(`add components to ${dir}`)
-    })
-  }
+  // if (!/README/.test(dir)) {
+  //   fs.appendFile(dir, `\n \n <comment-comment/> \n `, (err) => {
+  //     if (err) throw err
+  //     console.log(`add components to ${dir}`)
+  //   })
+  // }
+  fs.appendFile(dir, `\n \n <comment-comment/> \n `, (err) => {
+    if (err) throw err
+    console.log(`add components to ${dir}`)
+  })
 }
--- builds/findMarkdown.js
+++ builds/findMarkdown.js
@@ -4,17 +4,19 @@ const fs = require('fs')
 function findMarkdown(dir, callback) {
   fs.readdir(dir, function (err, files) {
     if (err) throw err
-
     files.forEach((fileName) => {
       let innerDir = `${dir}/${fileName}`
-
       if (fileName.indexOf('.') !== 0) {
         fs.stat(innerDir, function (err, stat) {
           if (stat.isDirectory()) {
             findMarkdown(innerDir, callback)
           } else {
-            // 跳过readme 文件，当然你也可以自行修改
-            if (/\.md$/.test(fileName) && !/README/.test(fileName)){
+            // 跳过所有README.md文件，当然你也可以自行修改
+            // if (/\.md$/.test(fileName) && !/README/.test(fileName)){
+            //   callback(innerDir);
+            // }
+            // 只跳过docs根目录下README.md文件
+            if (/\.md$/.test(fileName) && innerDir !== './docs/README.md'){
               callback(innerDir);
             }
           }
