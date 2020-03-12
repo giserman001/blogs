1b06e7bdbab44cbff44293a7663b157864cfc193
liuya <1522962872@qq.com>
2020-03-12 18:07:01 +0800

    feat: 添加打赏功能

---
+++ docs/.vuepress/theme/components/playTour.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id="money" :class="{ 'active': isClick, 'animate': true }" @click="clickMe">
+    <span></span>
+    <em class="close" @click.stop="closed"></em>
+    <div class="shadow"></div>
+    <ul>
+      <li>
+        <span>支付宝扫一扫、领红包</span>
+        <img src="https://slbblog.oss-cn-beijing.aliyuncs.com/getMoney.jpeg" />
+      </li>
+      <li>
+        <span>赏下了</span>
+        <img src="https://slbblog.oss-cn-beijing.aliyuncs.com/wechat.jpg" />
+      </li>
+      <li>
+        <span>支持一杯coffee</span>
+        <img src="https://slbblog.oss-cn-beijing.aliyuncs.com/coffee.jpeg" />
+      </li>
+      <li>
+        <span>赏下百两黄金：</span>
+        <img src="https://slbblog.oss-cn-beijing.aliyuncs.com/thankyou.jpeg" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isClick: false
+    }
+  },
+  methods: {
+    clickMe() {
+      this.isClick = true
+    },
+    closed() {
+      this.isClick = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+@keyframes ball {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-200px);
+  }
+}
+#money {
+  animation: ball 1.5s cubic-bezier(0.165, 0.84, 0.44, 1) infinite alternate;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+}
+
+#money > span {
+  cursor: pointer;
+  display: inline-block;
+  width: 65px;
+  height: 65px;
+  border-radius: 50%;
+  border: 3px solid #cd1b31;
+  background: url(https://slbblog.oss-cn-beijing.aliyuncs.com/nolook.svg);
+  background-size: cover;
+  animation: rotate 1.5s infinite alternate;
+}
+#money.active > span {
+  animation: none!important;
+  display: none!important;
+}
+#money.animate > span {
+  position: relative;
+}
+em {
+  font-style: italic;
+}
+#money ul {
+  display: none;
+}
+#money.active ul {
+  display: block!important;
+  overflow: hidden;
+}
+#money.active li {
+  width: 24%;
+  text-align: center;
+  list-style: none;
+  float: left;
+  margin-right: 10px;
+}
+#money.active li span {
+  display: block;
+  font-size: 13px;
+}
+#money.active img {
+  width: 150px;
+  border-radius: 10px;
+}
+#money.active>em {
+  width: 30px;
+  height: 30px;
+  right: 15px;
+  top: 8px;
+  cursor: pointer;
+  position: absolute;
+  display: inline-block;
+}
+#money.active {
+  box-shadow: 0 -3px 13px 0 #f1f1f1;
+  z-index: 1000;
+  bottom: 0!important;
+  width: 100%;
+  background: #fff;
+  animation: none!important;
+}
+#money.active>em:before {
+  content: "";
+  position: absolute;
+  background: #aaa;
+  height: 16px;
+  width: 1.5px;
+  top: 2px;
+  right: 9px;
+  border-radius: 1px;
+}
+#money.active>em:after{
+  content: "";
+  position: absolute;
+  background: #aaa;
+  height: 16px;
+  width: 1.5px;
+  top: 2px;
+  right: 9px;
+  border-radius: 1px;
+}
+#money.active>em:before {
+  transform: rotate(45deg);
+}
+#money.active>em:after {
+  transform: rotate(-45deg);
+}
+</style>
---
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="globalLayout">
+    <component :is="layout" />
+    <playTour />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { setGlobalInfo } from '@app/util'
+import playTour from '../components/playTour'
+export default {
+  name: 'GlobalLayout',
+  components: {
+    playTour
+  },
+  computed: {
+    layout () {
+      const layout = this.getLayout()
+      setGlobalInfo('layout', layout)
+      return Vue.component(layout)
+    }
+  },
+  methods: {
+    getLayout () {
+      if (this.$page.path) {
+        const layout = this.$page.frontmatter.layout
+        if (layout && (this.$vuepress.getLayoutAsyncComponent(layout)
+          || this.$vuepress.getVueComponent(layout))) {
+          return layout
+        }
+        return 'Layout'
+      }
+      return 'NotFound'
+    }
+  }
+}
+</script>
