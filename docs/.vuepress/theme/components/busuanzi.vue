0807599e3c49e1b026feebcce88ddb2fc66126f0
liuya <1522962872@qq.com>
2020-03-13 15:54:46 +0800

    fix: 不算子bug修复

--- docs/.vuepress/theme/components/busuanzi.vue
+++ docs/.vuepress/theme/components/busuanzi.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="busuanzi">
-    <span id="busuanzi_container_site_pv" style="display:none">
+    <span id="busuanzi_container_site_pv">
       本站总访问量
       <span id="busuanzi_value_site_pv"></span>次
       <span class="post-meta-divider">|</span>
     </span>
-    <span id="busuanzi_container_site_uv" style="display:none">
+    <span id="busuanzi_container_site_uv">
       本站访客数
       <span id="busuanzi_value_site_uv"></span>人
     </span>
@@ -14,16 +14,20 @@
  
 <script>
  
-const busuanzi = require("busuanzi.pure.js");
+const busuanzi = require("busuanzi.pure.js")
  
 export default {
-  // 监听,当路由发生变化的时候执行
-  watch: {
-    $route(to, from) {
-      if (to.path != from.path) {
-        script.fetch();
-      }
-    }
+  // // 监听,当路由发生变化的时候执行
+  // watch: {
+  //   $route(to, from) {
+  //     if (to.path != from.path) {
+  //       console.log('执行了吗？')
+  //       busuanzi.fetch();
+  //     }
+  //   }
+  // },
+  mounted() {
+    busuanzi.fetch()
   }
 };
 </script>
