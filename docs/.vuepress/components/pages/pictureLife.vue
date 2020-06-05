c07f192c065915aa9931c9cf6ce005d6ff3c9275
liuya <1522962872@qq.com>
2020-06-05 16:09:57 +0800

    feat: 图文生活页面完成

--- docs/.vuepress/components/pages/pictureLife.vue
+++ docs/.vuepress/components/pages/pictureLife.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="waterfallBox">
-    <vue-waterfall-easy :imgsArr="imgsArr">
+    <vue-waterfall-easy :imgsArr="imgsArr" @click="clickFn">
       <div class="img-info" slot-scope="props">
-        <p class="some-info">第{{ props.index + 1 }}张图片</p>
-        <p class="some-info">{{ props.value.info }}</p>
+        <div class="some-info">{{ props.value.info }}</div>
       </div>
     </vue-waterfall-easy>
+    <div class="show-div" v-if="sigleInfo.src" @click.stop="close">
+      <img :src="sigleInfo.src">
+      <div class="show-introduce">{{sigleInfo.info}}</div>
+    </div>
   </div>
 </template>
 
@@ -20,72 +23,78 @@ export default {
     return {
       imgsArr: [
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字1',
+          src: require('../../public/lifeImg/1.jpg'),
+          info: '童年的时光真好，看着女儿健康快乐成长，心里真的很开心。。。',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字2',
+          src: require('../../public/lifeImg/2.jpg'),
+          info: '宝贝女儿和漂亮妈妈臭美瞬间, 哈哈哈哈',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字3',
+          src: require('../../public/lifeImg/3.jpg'),
+          info: '毕业多年，12206寝室三杰再聚首，祝各位兄弟前程似锦。',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字4',
+          src: require('../../public/lifeImg/4.jpg'),
+          info: '女儿俏皮瞬间',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字1',
+          src: require('../../public/lifeImg/5.jpg'),
+          info: '一家三口野生动物一日游，跑了一天累死了。。。。',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字2',
+          src: require('../../public/lifeImg/6.jpg'),
+          info: '都感冒了，你还是那么调皮的玩耍。。。。',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字3',
+          src: require('../../public/lifeImg/7.jpg'),
+          info: '看着是不是很眼馋，大家知道是吗？ 哈哈哈',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字4',
+          src: require('../../public/lifeImg/8.jpg'),
+          info: '虽然已经离开老东家，但依旧祝福金棕榈长虹发展。',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字1',
+          src: require('../../public/lifeImg/9.jpg'),
+          info: '女儿生日，带她去拍了艺术照，她很喜欢，很开心。女儿照片基本不需要设计师ps,反而爸爸妈妈的照片需要ps操刀一番。。。',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字2',
+          src: require('../../public/lifeImg/10.jpg'),
+          info: '再见24号，再见黑曼巴',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字3',
+          src: require('../../public/lifeImg/11.jpg'),
+          info: '一家人去泡汤，宝贝女儿在里面玩疯了。。。',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字4',
+          src: require('../../public/lifeImg/12.jpg'),
+          info: '女儿在幼儿园的生活',
         },
         {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字1',
-        },
-        {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字2',
-        },
-        {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字3',
-        },
-        {
-          src: require('../../public/logo.jpg'),
-          info: '一些图片描述文字4',
-        },
+          src: require('../../public/lifeImg/13.jpg'),
+          info: '跟朋友去看上海车展，豪车如云，希望以后我也可以提一台豪车。。。',
+        }
       ],
+      sigleInfo: {}
     }
   },
+  methods: {
+    clickFn(event, { index, value }) {
+      // 阻止a标签跳转
+      event.preventDefault()
+      // 只有当点击到图片时才进行操作
+      if (event.target.tagName.toLowerCase() == 'img') {
+        this.sigleInfo = {
+          ...value
+        }
+      }
+    },
+    close(e) {
+      if(e.target.tagName.toLowerCase() !== 'img') {
+        this.sigleInfo = {}
+      }
+    }
+  }
 }
 </script>
 
@@ -100,5 +109,52 @@ export default {
 .waterfallBox >>> .vue-waterfall-easy-container .vue-waterfall-easy-scroll{
   overflow-x: visible;
   overflow-y: visible;
+}
+.waterfallBox >>> .img-inner-box{
+  position relative
+  cursor pointer
+}
+.waterfallBox >>> .img-inner-box:hover .img-info{
+  display block
+}
+.img-info{
+  position absolute
+  bottom 0
+  left 0
+  width 100%
+  z-index 1
+  height: auto;
+  padding: 6px;
+  background-color: rgba(255, 255, 255, 0.85);
+  color: rgba(0, 0, 0, 0.65);
+  font-size: 14px;
+  display none
+}
+.show-div {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0px;
+  top: 0px;
+  background-color: rgba(0,0,0,.8);
+  text-align: center;
+  padding: 30px;
+  color: #fff;
+  z-index 100
+}
+.show-div > img {
+  border-radius: 4px;
+  max-width: 90%;
+  max-height 80%
+}
+img {
+  vertical-align: middle;
+  border-style: none;
+}
+.show-introduce {
+  margin: 20px auto;
+  font-size: 1.4rem;
+  max-width: 800px;
+  text-align: center;
 }
 </style>
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,9 @@
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import copy from "./common/copy";
+// 暂时没有用到这个组件,以后可能会用到，暂时不删除（图片预览组件）
+import preview from 'vue-photo-preview'
+import 'vue-photo-preview/dist/skin.css'
 
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
@@ -9,6 +12,7 @@ export default ({
   siteData // 站点元数据
 }) => {
   Vue.use(Element)
+  Vue.use(preview)
   setTimeout(() => {
     try {
       document &&
